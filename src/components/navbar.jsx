b42f4237b23c04b74926c40072ec5f9e3d385a30
Rajat Saxena <rajasa@asiapac.altair.com>
2018-09-24 15:48:26 +0530

    changes in the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ totalCount }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        Navbar{" "}
+        Navbar hello{" "}
         <span className="badge badge-secondary badge-pill">{totalCount}</span>
       </a>
     </nav>
