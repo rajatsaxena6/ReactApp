6d12a7818cd8e656ba0c0dcc69c84fc57104ceea
Rajat Saxena <rajasa@asiapac.altair.com>
2018-09-24 11:57:46 +0530

    Added new test case for navbar component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Navbar from "./components/navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("shows that the navbar component is also present", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Navbar />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
